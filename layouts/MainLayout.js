8eae49fb5c87351ded00f727fe6a0abe305911a3
anungbisa4 <anung.bisa4@gmail.com>
2021-07-17 12:53:15 +0700

    bug fix header

--- layouts/MainLayout.js
+++ layouts/MainLayout.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Navbar } from "@/components/Header";
+import Navbar from "@/components/Header/Navbar";
 import { Sidebar } from "@/components/Sidebar";
 
 const MainLayout = ({ children }) => {
